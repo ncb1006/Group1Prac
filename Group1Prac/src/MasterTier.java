baf8b2467eacf824790d6f106d8e8378eef17a19
barth <barth@LAPTOP-59AUFU12>
2022-02-08 13:01:21 -0500

    ...Connor First Commit

--- Group1Prac/src/MasterTier.java
+++ Group1Prac/src/MasterTier.java
@@ -4,6 +4,8 @@ public class MasterTier {
 	public static void main(String[] args) {
 		String accName = "Narthen";
 		System.out.println(accName);
+		
+		System.out.println("Conflo is better, could be GM probs");
 
 	}
 
